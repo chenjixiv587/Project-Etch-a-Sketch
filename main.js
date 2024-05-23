fd7d1d4953e38bbaa6a137a83169b318b9046ad8
chenjixiv587 <lb5077735@gmail.com>
2024-05-23 13:23:55 +0800

    change the squares color

--- main.js
+++ main.js
@@ -3,32 +3,43 @@ const container = document.querySelector(".container")
 const containerWidth = container.offsetWidth;
 const askNumber = document.querySelector("button");
 let divPerSide = 16;
-
-const generateNewDiv = (perSideDivs) => {
-    let divWidth = containerWidth / perSideDivs;
-    let divHeight = containerWidth / perSideDivs;
-    for (let i = 0; i < perSideDivs * perSideDivs; i++) {
-        const div = document.createElement("div");
-        div.setAttribute("style", `width: ${divWidth}px; height: ${divHeight}px; border: 1px solid #ccc`);
-        container.appendChild(div);
-    }
-}
-generateNewDiv(16);
-
 const generateRandomColor = () => {
     let red = Math.floor(Math.random() * 255 + 1);
     let green = Math.floor(Math.random() * 255 + 1);
     let blue = Math.floor(Math.random() * 255 + 1);
     return `rgb(${red}, ${green}, ${blue})`;
 }
+const generateRandomColorLittle = () => {
+    let red = Math.floor(Math.random() * 255 + 1);
+    let green = Math.floor(Math.random() * 255 + 1);
+    let blue = Math.floor(Math.random() * 255 + 1);
+    let randomNum = Math.random() * 0.5;
+    return `rgba(${red}, ${green}, ${blue}, ${randomNum})`;
+}
+const generateNewDiv = (perSideDivs) => {
+    let divWidth = containerWidth / perSideDivs;
+    let divHeight = containerWidth / perSideDivs;
+    for (let i = 0; i < perSideDivs * perSideDivs; i++) {
+        const div = document.createElement("div");
+        div.setAttribute("style", `width: ${divWidth}px; height: ${divHeight}px; border: 1px solid #ccc;`);
+        container.appendChild(div);
+    }
+}
+generateNewDiv(16);
+
+
 //When only container exists, the backgroundColor is #eee;
 container.addEventListener("mouseover", (e) => {
     if (e.target === e.currentTarget) {
-        e.target.style.backgroundColor = "#eee";
+        e.target.style.backgroundColor = "#fff";
     }
     else {
         e.target.style.backgroundColor = generateRandomColor();
     }
+})
+
+container.addEventListener("mouseout", (e) => {
+    e.target.style.backgroundColor = "#fff";
 })
 
 askNumber.addEventListener("click", (e) => {
