86d13810f30cdc5e9364e247f034734dd61ed884
chenjixiv587 <lb5077735@gmail.com>
2024-05-23 10:48:24 +0800

    Mouse over change the div backgroundColor

--- main.js
+++ main.js
@@ -9,3 +9,12 @@ for (let i = 0; i < divPerSide * divPerSide; i++) {
     div.setAttribute("style", `width: ${divWidth}px; height: ${divHeight}px; border: 1px solid #ccc`);
     container.appendChild(div);
 }
+const generateRandomColor = () => {
+    let red = Math.floor(Math.random() * 255 + 1);
+    let green = Math.floor(Math.random() * 255 + 1);
+    let blue = Math.floor(Math.random() * 255 + 1);
+    return `rgb(${red}, ${green}, ${blue})`;
+}
+container.addEventListener("mouseover", (e) => {
+    e.target.style.backgroundColor = generateRandomColor();
+})
